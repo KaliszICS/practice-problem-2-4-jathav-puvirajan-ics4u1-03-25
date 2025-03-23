6420c7458b9571cd75aa595d54dfcc0c81ee0b85
jathav-puvirajan-ics4u1-03-25 <jathav.puvirajan@student.tdsb.on.ca>
2025-03-23 18:20:12 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,7 +3,39 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
+	public static void selectionSortName(String[] names, int[] ages){
+		for(int i = 0; i < names.length - 1; i++){
+			int sI = i;
+			for(int j = i + 1; j < names.length; j++){
+				if(names[sI].compareToIgnoreCase(names[j]) > 0){
+					sI = j;
+				}
+			}
 
-	
+			String tempString = names[sI];
+			int tempInt = ages[sI];
+			names[sI] = names[i];
+			ages[sI] = ages[i];
+			names[i] = tempString;
+			ages[i] = tempInt;
+		}
+	}
+	public static void selectionSortAge(String[] names, int[] ages){
+		for(int i = 0; i < names.length - 1; i++){
+			int sI = i;
+			for(int j = i + 1; j < names.length; j++){
+				if(ages[sI] > ages[j]){
+					sI = j;
+				}
+			}
+
+			String tempString = names[sI];
+			int tempInt = ages[sI];
+			names[sI] = names[i];
+			ages[sI] = ages[i];
+			names[i] = tempString;
+			ages[i] = tempInt;
+		}
+	}
 
 }
